03b164864cc3be1af89427ae371e4db40f7296b5
Kino <danikkinaddd3@mail.ru>
2022-06-10 18:47:08 +0300

    Lesson2

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 const goods = [
-    { /*photo: document.img.src = 'img/Shirt.jpg',*/ title: 'Shirt', price: 150 },
+    { title: 'Shirt', price: 150 },
     { title: 'Socks', price: 50 },
     { title: 'Jacket', price: 350 },
     { title: 'Shoes', price: 250 },
 ];
-const renderGoodsItem = ({ /*photo = ' ',*/ title = ' ', price = 0 }) => {
-    return `<div class="goods-item">
-    <h3>${title}</h3>
-    <p>${price}</p>
-    </div>`;
-};
-const renderGoodsList = (list = []) => {
-    let goodsList = list.map(item => renderGoodsItem(item)).join('');
-    document.querySelector('.goods-list').innerHTML = goodsList;
+
+class GoodsItem {
+    constructor({ title = ' ', price = 0 }) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `
+      <div class="goods-item">
+        <h3>${this.title}</h3>
+        <p>${this.price}</p>
+      </div>
+    `;
+    }
 }
-renderGoodsList(goods);
+class GoodsList {
+    item = []
+    fetchGoods() {
+        this.items = goods;
+    }
+    /*getCount() {
+        let totalPrice = document.getElementById('goods-list__price');
+        let sum = 0;
+        this.goods.forEach(goods => {
+            sum += goods.price
+        });
+        totalPrice.innerText = `Итого  ${sum} рублей`;
+    }*/ // Я просто не понимаю из-за чего, при добавления данного метода, все перестает работать
+    render() {
+        const goods = this.items.map(item => {
+            const goodItem = new GoodsItem(item);
+            return goodItem.render()
+        }).join('');
+        document.querySelector('.goods-list').innerHTML = goods;
+    }
+}
+
+const goodsList = new GoodsList();
+goodsList.fetchGoods();
+//goodsList.getCount();
+goodsList.render();
