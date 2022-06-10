6408e5d4d711c280a7af2f156c12ce0fca118a5c
Kino <danikkinaddd3@mail.ru>
2022-06-11 01:25:18 +0300

    Lesson2

--- script.js
+++ script.js
@@ -6,24 +6,32 @@ const goods = [
 ];
 
 class GoodsItem {
-    constructor({ title = ' ', price = 0 }) {
+    constructor(title, price) {
         this.title = title;
         this.price = price;
     }
     render() {
         return `
-      <div class="goods-item">
+        <div class="goods-item">
         <h3>${this.title}</h3>
         <p>${this.price}</p>
-      </div>
-    `;
+        </div>`;
     }
 }
+
 class GoodsList {
-    item = []
-    fetchGoods() {
-        this.items = goods;
+    constructor() {
+        this.goods = [];
     }
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ];
+    }
+
     /*getCount() {
         let totalPrice = document.getElementById('goods-list__price');
         let sum = 0;
@@ -33,15 +41,15 @@ class GoodsList {
         totalPrice.innerText = `Итого  ${sum} рублей`;
     }*/ // Я просто не понимаю из-за чего, при добавления данного метода, все перестает работать
     render() {
-        const goods = this.items.map(item => {
-            const goodItem = new GoodsItem(item);
-            return goodItem.render()
-        }).join('');
-        document.querySelector('.goods-list').innerHTML = goods;
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
     }
 }
 
-const goodsList = new GoodsList();
-goodsList.fetchGoods();
-//goodsList.getCount();
-goodsList.render();
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
