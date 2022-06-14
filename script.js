950e5b6f665024cfc3c2346639eb711d93af8c05
Kino <danikkinaddd3@mail.ru>
2022-06-14 19:46:42 +0300

    Lesson3

--- script.js
+++ script.js
@@ -5,33 +5,39 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
 
+const GET_GOODS_ITEMS = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json'
+const GET_BASKET_GOODS_ITEMS = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json'
+
+function service(url, callback) {
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.onload = () => {
+        callback(JSON.parse(xhr.response))
+    }
+}
 class GoodsItem {
-    constructor(title, price) {
-        this.title = title;
+    constructor({ product_name, price }) {
+        this.product_name = product_name;
         this.price = price;
     }
     render() {
         return `
         <div class="goods-item">
-        <h3>${this.title}</h3>
+        <h3>${this.product_name}</h3>
         <p>${this.price}</p>
         </div>`;
     }
 }
 
 class GoodsList {
-    constructor() {
-        this.goods = [];
+    items = [];
+    fetchGoods(callback) {
+        service(GET_GOODS_ITEMS, (data) => {
+            this.items = data;
+            callback()
+        });
     }
-    fetchGoods() {
-        this.goods = [
-            { title: 'Shirt', price: 150 },
-            { title: 'Socks', price: 50 },
-            { title: 'Jacket', price: 350 },
-            { title: 'Shoes', price: 250 },
-        ];
-    }
-
     /*getCount() {
         let totalPrice = document.getElementById('goods-list__price');
         let sum = 0;
@@ -40,17 +46,35 @@ class GoodsList {
         });
         totalPrice.innerText = `Итого  ${sum} рублей`;
     }*/ // Я просто не понимаю из-за чего, при добавления данного метода, все перестает работать
+    getCount() {
+        return this.items.reduce((prev, { price }) => {
+            return prev + price;
+        }, 0)
+    }
     render() {
         let listHtml = '';
-        this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price);
-            listHtml += goodItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = listHtml;
+        const goods = this.items.map(item => {
+            const goodItem = new GoodsItem(item);
+            return goodItem.render()
+        }).join('');
+        document.querySelector('.goods-list').innerHTML = goods;
     }
 }
 
-const list = new GoodsList();
-list.fetchGoods();
-// list.getCount();
-list.render();
+class BasketGoods {
+    items = [];
+    fetchGoods(callback = () => { }) {
+        service(GET_BASKET_GOODS_ITEMS, (data) => {
+            this.items = data;
+            callback()
+        });
+    }
+}
+
+const basketGoods = new BasketGoods();
+basketGoods.fetchGoods()
+
+const goodsList = new GoodsList();
+goodsList.fetchGoods(() => {
+    goodsList.render();
+});
